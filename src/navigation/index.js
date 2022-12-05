660fe145930105894ec3d7405b068ff0a483b95e
Ivan Shitikov <ifinesse2406@gmail.com>
2022-12-05 16:44:10 +0200

    added navigation

---
+++ src/navigation/index.js
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/Login';
+import SignUpScreen from '../screens/SignUp';
+import EditScreen from '../screens/Edit';
+
+
+const Stack = createNativeStackNavigator();
+
+export default RootNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Edit" component={EditScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
